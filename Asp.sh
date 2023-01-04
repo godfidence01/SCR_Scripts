a33adbed861526819dc274ee17391d8140c2e171
Ramneek <33387970+godfidence01@users.noreply.github.com>
2023-01-04 13:17:25 +0530

    Add files via upload

---
+++ Asp.sh
@@ -0,0 +1,80 @@
+   1  grep -ir "getpartnerpath"
+    2  grep -ir "getpartnerpath" *
+    3  grep -ir "getpartnerapipath" *
+    4  grep -ir "basicurl" *
+    5  grep -ir "set-cookie" * -n
+    6  grep -ir "httpcookie" * -n
+    7  grep -ir "Extracttofile" * -n
+    8  grep -ir "compileassemblyfromsource" * -n
+    9  grep -ir "response.headers" * -n
+   10  grep -ir "xmlserializer" * -n
+   11  grep -ir "response.write" * -n
+   12  grep -ir "response.write" * -n
+   13  grep -ir "cosetproxyBlanket" * -n
+   14  grep -ir "executesqlcommand" * -n
+   15  grep -ir "executeSqlcommand" * -n
+   16  grep -ir "system.io.file.delete" * -n
+   17  grep -ir "ds.filter" * -n
+   18  grep -ir "p/startinfo.filename" * -n
+   19  grep -ir "p.startinfo.filename" * -n
+   20  grep -ir "jwt.finance" * -n
+   21  grep -ir "tripleDESCryptoserviceprovider" * -n
+   22  grep -ir "rc2cryptoserviceprovider" * -n
+   23  grep -ir "ciphermode" * -n
+   24  grep -ir "path.gettempfilename" * -n
+   25  grep -ir "authenticationtyoes" * -n
+   26  grep -ir "authenticationtypes" * -n
+   27  grep -ir "keysize" * -n
+   28  grep -ir "md4" * -n
+   29  grep -ir "md5" * -n
+   30  grep -ir "SHA1" * -n
+   31  grep -ir "deserializer" * -n
+   32  grep -ir "webrequest.create" * -n
+   33  grep -ir "securitycafecritical" * -n
+   34  exit
+   35  grep -ir "console.writeline(" *
+   36  grep -ir "console.writeline(" *
+   37  grep -ir "response.add" *
+   38  grep -ir "serializationinfo" *
+   39  grep -ir "system.web.mvc*" *
+   40  grep -ir "startinfo*" *
+   41  grep -ir "securitysafecritical*" *
+   42  grep -ir "webrequest.create*" *
+   43  grep -ir "sslprotocols" *
+   44  grep -ir "directoryentry" *
+   45  grep -ir "uselegactforms" *
+   46  grep -ir "uselegacyforms" *
+   47  grep -ir "acceptwebsocket" *
+   48  grep -ir "enableheaderchecking" *
+   49  grep -ir "process.start" *
+   50  grep -ir "request.form" *
+   51  grep -ir "httpcookiw" *
+   52  grep -ir "httpcookie" *
+   53  grep -ir "samesite" *
+   54  grep -ir "cookie.domain" *
+   55  grep -ir "requiressl" *
+   56  grep -ir "require.ssl" *
+   57  grep -ir "server.htmmlencode" *
+   58  grep -ir "server.htmlencode" *
+   59  grep -ir "readline(" *
+   60  grep -ir "allowinsecuredeserialization" *
+   61  grep -ir "allowinsecure" *
+   62  grep -ir "useunsafeheader" *
+   63  grep -ir "password" *
+   64  grep -ir "password=" *
+   65  grep -ir "hmac.create" *
+   66  grep -ir "encryptionkey" *
+   67  grep -ir "LDAP" *
+   68  grep -ir "createquery" *
+   69  grep -ir "allowrelaxedrelativeurl" *
+   70  history
+   71  grep -ir "basicurl" *
+   72  grep -ir "basicurl" *
+   73  grep -ir "base64" -n P
+   74  grep -ir "base64" -n *
+   75  grep -ir "sqldataadapter" -n *
+   76  grep -ir "executereader" -n *
+   77  grep -ir "factory.setFeature" *
+   78  grep -ir "documentbuilderfactory" *
+
+Reference : https://vulncat.fortify.com/en/weakness?kingdom=Input+Validation+and+Representation%3bSecurity+Features&codelang=C%23%2fVB.NET%2fASP.NET%3bVisualBasic%2fVBScript%2fASP&po=11
